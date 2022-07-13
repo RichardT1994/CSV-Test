ae56735e6e100baa4c2a2dbc84dd9c6c5f6d1d51
RichardT1994 <96494628+RichardT1994@users.noreply.github.com>
2022-07-13 14:40:20 -0400

    added notes
    Notes added

--- CSV Test/Program.cs
+++ CSV Test/Program.cs
@@ -5,12 +5,12 @@ namespace CSV_Test
     class Program
     {
         static void Main(string[] args)
-        {
+        { // test data don't 
             string fname = "Rick";
             string lname = "Tucker";
             string email = "test";
             string address = "test street";
-            string hphone ="304-123-1000";
+            string hphone = "304-123-1000";
             string counties = "Kanawha";
             string route = "52";
             string rdname = "taco";
@@ -19,16 +19,17 @@ namespace CSV_Test
             string description = "Read the map morty";
             string city = "Fresh";
 
+            //******!!!!!If you are importing this into your project only copy from here down!!!!**********
             createlog(DateTime.Now.ToString(), fname, lname, email, address, hphone, counties, route, rdname, landmark, gps, description, city);
             static void createlog(string date, string fname, string lname, string email, string address, string hphone, string counties, string route, string rdname, string landmark, string gps, string description, string city)
             {
                 try
                 {
                     DateTime dateOnly = DateTime.Today;
-                    
 
+                    // This should be your file destination and what you want the file to be named. 
                     string filepath = @"\\dotb6ftp02v\d$\SWATLogs\" + dateOnly.ToString("MM.dd.yyyy") + "testSwatLog.csv";
-                   // string filepath = @"\\dotb6ftp02v\d$\SWATLogs\SwatLog.csv";
+                    // string filepath = @"\\dotb6ftp02v\d$\SWATLogs\SwatLog.csv";
                     using (System.IO.StreamWriter file = new System.IO.StreamWriter(filepath, true))
                     {   // Line below writes the data to the csv file
                         file.WriteLine(date + "," + fname + "," + lname + "," + email + "," + address + "," + hphone + "," + counties + "," + route + "," + rdname + "," + landmark + "," + gps + "," + description + "," + city);
@@ -40,19 +41,5 @@ namespace CSV_Test
                 }
             }
         }
-        /*public static void addRecord(string ID, string name, string age, string filepath)
-            {
-                try
-                {
-                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filepath, true))
-                    {
-                    file.WriteLine(ID + "," + name + "," + age);
-                    }
-                }
-                catch(Exception ex)
-                {
-                throw new ApplicationException("Crashed", ex);
-                }
-            }*/     
     }
 }
